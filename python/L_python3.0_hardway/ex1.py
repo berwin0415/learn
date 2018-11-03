94a8279a94b29765b8dc171e32e48033728aabb1
hanbowen <berwin0415@163.com>
2018-11-03 21:48:51 +0800

    aaa

---
+++ python/L_python3.0_hardway/ex1.py
@@ -0,0 +1,9 @@
+#aa
+
+print("Hello Word")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I`d much rather you 'not'.")
+print('I "said" do not touch this')
---
+++ python/L_python3.0_hardway/ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:") #打印文字
+
+print("Hens", 25.0 + 30.0 / 6.0) #计算 先30/6 得5 加25 得30
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0) # 计算 先 25*3 得75 对4取余 得3 100-3得97
+
+print("Now I will count the eggs:") # 打印文字
+
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0) # 计算  4余2 得0 1/4 得 0.25 3加2加1减5加0 减0.25加6 得 6.75
+
+print("Is it true that 3 + 2 < 5 - 7?")# 打印文字
+
+print(3.0 + 2.0 < 5.0 - 7.0) # 判断 5 < -2 false
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
