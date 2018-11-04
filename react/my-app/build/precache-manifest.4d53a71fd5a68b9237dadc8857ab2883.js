572522f21bfdfc47840dbb05d35815216998651b
berwin han <berwin0415@gmail.com>
2018-11-04 12:39:16 +0800

    jenkins node 测试

---
+++ react/my-app/build/precache-manifest.4d53a71fd5a68b9237dadc8857ab2883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b2eca8cf996e9843b40a",
+    "url": "/static/js/main.b2eca8cf.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "b2eca8cf996e9843b40a",
+    "url": "/static/css/main.05bc9bc7.chunk.css"
+  },
+  {
+    "revision": "5556570a59f68e149d1915a0d203b18d",
+    "url": "/index.html"
+  }
+];
