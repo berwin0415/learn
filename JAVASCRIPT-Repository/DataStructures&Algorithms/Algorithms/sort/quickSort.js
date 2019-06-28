919ddf46b1782d8d557b97627541f6e324a05d65
berwin <berwin0415@gmail.com>
2019-06-28 18:42:14 +0800

    快排

---
+++ JAVASCRIPT-Repository/DataStructures&Algorithms/Algorithms/sort/quickSort.js
@@ -0,0 +1,54 @@
+// 快速排序
+
+// const quickSort = arr => {
+//     const sort = (arr) => {
+//         const len = arr.length
+//         if(len < 2) {return arr}
+
+//         let flag = arr[0]
+//         let left = [];
+//         let right = [];
+
+//         for (let i = 1; i < len; i++) {
+//             if (arr[i]  < flag) {
+//                 left.push(arr[i])
+//             }else {
+//                 right.push(arr[i])
+//             }
+//         }
+
+//         return sort(left).concat(flag,sort(right))
+
+//     }
+//     return sort(arr)
+// }
+
+const quickSort = arr => {
+    // 找到数组标尺元素下角标
+    const findCenter = (arr, left, right) => {
+        let flag = arr[left]
+        let idx = left + 1;
+        for (let i = idx; i <= right; i++) {
+            if (arr[i] < flag) {
+                [arr[i], arr[idx]] = [arr[idx], arr[i]]
+                idx++
+            }
+        }
+        [arr[idx - 1], arr[left]] = [arr[left], arr[idx - 1]]
+        return idx
+    }
+    const sort = (arr, left, right) => {
+        if (left < right) {
+            const center = findCenter(arr, left, right);
+            sort(arr, left, center - 1)
+            sort(arr, center, right)
+        }
+    }
+    sort(arr, 0, arr.length - 1)
+    return arr;
+}
+const data = Array(1000).fill(1).map((item, index) => 10000 - index)
+const start = new Date();
+console.log(quickSort(data))
+const end = new Date()
+console.log(end - start)
