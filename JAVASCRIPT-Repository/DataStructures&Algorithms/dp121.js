6324e87682f5b2372b9321c17effd787a6005662
berwin <berwin0415@gmail.com>
2020-05-27 20:33:09 +0800

    update dp

---
+++ JAVASCRIPT-Repository/DataStructures&Algorithms/dp121.js
@@ -0,0 +1,19 @@
+const maxProfit = function (prices) {
+  const n = prices.length;
+  const dp = Array.from(new Array(n), () => new Array(2).fill(0));
+  if (n === 0) {
+    return 0;
+  }
+  for (let i = 0; i < n; i++) {
+    if (i === 0) {
+      dp[i][0] = 0;
+      dp[i][1] = -prices[i];
+      continue;
+    }
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + parseInt(prices[i]));
+    dp[i][1] = Math.max(dp[i - 1][1], -parseInt(prices[i]));
+  }
+  return dp[n - 1][0];
+};
+
+console.log(maxProfit([7, 1, 5, 6, 4, 3]));
---
+++ JAVASCRIPT-Repository/DataStructures&Algorithms/dp123.js
@@ -0,0 +1,31 @@
+const maxProfit = function (prices) {
+  const n = prices.length;
+  const k = 2;
+  const dp = Array.from(new Array(n), () =>
+    Array.from(new Array(k + 1), () => new Array(2).fill(0))
+  );
+  if (n === 0) {
+    return 0;
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = k; j >= 1; j--) {
+      if (i === 0) {
+        dp[i][j][0] = 0;
+        dp[i][j][1] = -prices[i];
+        continue;
+      }
+      dp[i][j][0] = Math.max(
+        dp[i - 1][j][0],
+        dp[i - 1][j][1] + parseInt(prices[i])
+      );
+      dp[i][j][1] = Math.max(
+        dp[i - 1][j][1],
+        dp[i - 1][j - 1][0] - parseInt(prices[i])
+      );
+    }
+  }
+  console.log(dp);
+  return dp[n - 1][k][0];
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 3]));
