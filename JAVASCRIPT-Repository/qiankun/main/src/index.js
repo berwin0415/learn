1fea5d1179750ddca2012c2ae332da38348c1b12
berwin <berwin0415@gmail.com>
2020-07-31 10:41:37 +0800

    create

--- JAVASCRIPT-Repository/qiankun/main/src/index.js
+++ JAVASCRIPT-Repository/qiankun/main/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { registerMicroApps, start } from "qiankun";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+registerMicroApps([
+  {
+    name: "react app", // app name registered
+    entry: "//localhost:7100",
+    container: "#yourContainer",
+    activeRule: "/demo1",
+  },
+  {
+    name: "vue app",
+    entry: "//localhost:7102",
+    container: "#yourContainer2",
+    activeRule: "/demo2",
+  },
+]);
+start();
