785edbb8667b480835e2f131094cc17f530b0a9f
berwin <berwin0415@gmail.com>
2021-01-25 10:17:00 +0800

    update: tree

--- JAVASCRIPT-Repository/DataStructures&Algorithms/Algorithms/sort/quickSort.js
+++ JAVASCRIPT-Repository/DataStructures&Algorithms/Algorithms/sort/quickSort.js
@@ -23,46 +23,63 @@
 //     return sort(arr)
 // }
 class a {
-    constructor(){
-        const obj = {
-            on:function (args) {
-                
-            },
-            publish:function (name, args) {
-                
-            }
-        }
-
-    }
+  constructor() {
+    const obj = {
+      on: function (args) {},
+      publish: function (name, args) {},
+    };
+  }
 }
 
-const quickSort = arr => {
-    // 找到数组标尺元素下角标
-    const findCenter = (arr, start, end) => {
-        let flag = arr[start]
-        let idx = start + 1;
-        for (let i = idx; i <= end; i++) {
-            if (arr[i] < flag) {
-                [arr[i], arr[idx]] = [arr[idx], arr[i]]
-                idx++
-            }
-        }
-        [arr[idx - 1], arr[start]] = [arr[start], arr[idx - 1]]
-        return idx
+const quickSort = (arr) => {
+  // 找到数组标尺元素下角标
+  const findCenter = (arr, start, end) => {
+    let flag = arr[start];
+    let idx = start + 1;
+    for (let i = idx; i <= end; i++) {
+      if (arr[i] < flag) {
+        [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        idx++;
+      }
     }
-    // 递归排序，直到左右指针相等为止
-    const sort = (arr, left, right) => {
-        if (left < right) {
-            const center = findCenter(arr, left, right);
-            sort(arr, left, center - 1)
-            sort(arr, center, right)
-        }
+    [arr[idx - 1], arr[start]] = [arr[start], arr[idx - 1]];
+    return idx;
+  };
+  // 递归排序，直到左右指针相等为止
+  const sort = (arr, left, right) => {
+    if (left < right) {
+      const center = findCenter(arr, left, right);
+      sort(arr, left, center - 1);
+      sort(arr, center, right);
     }
-    sort(arr, 0, arr.length - 1)
-    return arr;
-}
-const data = Array(1000).fill(1).map((item, index) => 10000 - index)
+  };
+  sort(arr, 0, arr.length - 1);
+  return arr;
+};
+const data = Array(100)
+  .fill(1)
+  .map((item, index) => 100 - index);
 const start = new Date();
-console.log(quickSort(data))
-const end = new Date()
-console.log(end - start)
+// console.log(quickSort(data));
+const end = new Date();
+// console.log(end - start);
+const quickSort2 = (arr) => {
+  if (arr.length > 1) {
+    const centerIndex = Math.floor(arr.length / 2);
+    const center = arr.splice(centerIndex,1)[0]
+    const left = []
+    const right = []
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (element > center) {
+            right.push(element)
+        }else{
+            left.push(element)
+        }
+    }
+    return quickSort2(left).concat([center],quickSort2(right))
+  }
+  return arr;
+};
+
+console.log(quickSort2(data))
