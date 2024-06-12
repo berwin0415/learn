2ea820157403a6fa4c7c2f8bd5bc317a965d81ed
hanbowen01 <hanbowen01@WorkMacPro.local>
2024-06-12 19:09:26 +0800

    add cpp

---
+++ cpp/src/02.variable/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int variable = 10; // -2b -> 2b
+    // std::cout << variable << std::endl;
+    std::cout << sizeof(long long) << std::endl;
+
+    std::cin.get();
+}
